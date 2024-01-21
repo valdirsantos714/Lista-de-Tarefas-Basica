61bcc30b3bb580631a5b2e9e67142ea8016170fe
Valdir Santos <valdirsantost40@gmail.com>
2024-01-21 17:33:22 -0300

    Retirado as anotações

--- script.js
+++ script.js
@@ -4,33 +4,12 @@ let butao = document.getElementById("butao")
 
 let lista = document.getElementById("lista")
 
-//let remover = document.getElementById("remover")
-
-//let li;
-
-//remover.style.display = "none"
-
-/*function criarLi() {
-    
-
-    let li = document.createElement("li")
-}*/
-
-
 butao.addEventListener("click", function () {
     let li = document.createElement("li")
     li.innerHTML = input.value;
     lista.appendChild(li);
     input.value = ""
-    /*remover.style.display = "inline-block"*/
 })
-
-/*
-remover.addEventListener("click", function () {
-
-    lista.remove(li)
-    remover.style.display = "none"
-})*/
 
 input.addEventListener("keydown", function (evento) {
 
